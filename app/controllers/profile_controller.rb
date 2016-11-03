956824e543105d282a952305d1560505cee010d4
Jacob <jacob-veal@uiowa.edu>
2016-11-03 14:13:05 +0000

    profile tab and view added

---
+++ app/controllers/profile_controller.rb
@@ -0,0 +1,19 @@
+class ProfileController < ApplicationController
+  before_action :authenticate_user!
+
+   def new
+   end
+
+   def show
+      
+   end
+
+   def create
+     
+   end
+   
+   def index
+       
+   end
+
+end
