b1e2266a7fc586eeb11f9b3bd27769bc64efca19
Jacob <jacob-veal@uiowa.edu>
2016-11-07 19:39:32 +0000

    firebase connection figured out

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -16,8 +16,21 @@ class ProfileController < ApplicationController
     puts "bank"
     render "bank"
   end
+  
+  def create
+    test_firebase
+    render "bank"
+  end
    
   def property
      @properties = Property.all
   end
+  
+  def test_firebase
+    response = FIREBASE.push("Owners", { :name => 'Test', :priority => 1 })
+    if(response.success?)
+      puts "response success"
+    end
+  end
+  
 end
