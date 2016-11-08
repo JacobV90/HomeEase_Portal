7a79d2983053db7bd68ad3e3536c26d490748325
Sam Glenn <samglennss3@gmail.com>
2016-11-08 18:49:32 +0000

    fixed the migration issues, migration is a real pain

--- db/schema.rb
+++ db/schema.rb
@@ -10,29 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161107212146) do
+ActiveRecord::Schema.define(version: 20161108184829) do
 
   create_table "properties", force: :cascade do |t|
-    t.string   "address"
-    t.string   "description"
-    t.datetime "created_at",                                                                                                                                   null: false
-    t.datetime "updated_at",                                                                                                                                   null: false
-    t.integer  "rent_amount",            default: 1000
-    t.integer  "number_bedrooms",        default: 0
-    t.integer  "number_bathrooms",       default: 0
-    t.string   "amenities",              default: "You can list notable things about your property here, such as if pets are allowed or parking is available"
-    t.string   "email",                  default: "",                                                                                                          null: false
-    t.string   "encrypted_password",     default: "",                                                                                                          null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,                                                                                                           null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.index ["email"], name: "index_properties_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_properties_on_reset_password_token", unique: true
+    t.string  "address",           default: "666 Fluffy Bunny Way, TooCool4School",                                                                      null: false
+    t.integer "rent_amount",       default: 1000
+    t.integer "integer",           default: 0
+    t.integer "number_bathrooms",  default: 0
+    t.integer "number_bedrooms",   default: 0
+    t.string  "amenities",         default: "You can list notable things about your property here, such as if pets are allowed or parking is available"
+    t.string  "description",       default: ""
+    t.boolean "available_to_rent", default: true
   end
 
   create_table "users", force: :cascade do |t|
