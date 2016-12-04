667ae19060df306e5887ce18dcb165daf4d52705
Sam Glenn <samglennss3@gmail.com>
2016-12-04 00:16:45 +0000

    making pages look pretty for about page and default images to properties

--- app/assets/javascripts/properties.js
+++ app/assets/javascripts/properties.js
@@ -6,4 +6,19 @@ $(function() {
     });
 });
 
+function getThumbnail(name)
+{
+    var image_url = name ;
+
+    $.get(image_url)
+        .done(function()
+        {
+            return image_url;
+        })
+        .fail(function()
+        {
+            return "HomeeaseSkyline";
+        })
+}
+
 
