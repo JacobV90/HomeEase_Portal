c9aa73f044122f9b989dae30f497166d3649be44
Yuhaw Richard Su <yuhaw-su@uiowa.edu>
2016-12-10 04:08:26 +0000

    added some tests

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -10,16 +10,29 @@ When /^I have attempted to register the user "(.*?)" "(.*?)" with email "(.*?)" 
   fill_in 'user_phone_number', :with => '123-456-7890'
   fill_in 'user_password', :with => password
   fill_in 'user_password_confirmation', :with => password
-  click_button 'Save Changes'
+  click_button 'Sign up'
 end
 
-Then /^I should see a movie list entry with title "(.*?)" and rating "(.*?)"$/ do |title, rating| 
-  result=false
-  all("tr").each do |tr|
-    if tr.has_content?(title) && tr.has_content?(rating)
-      result = true
-      break
-    end
-  end  
-  expect(result).to be_truthy
+Then /^I should be signed in as "(.*?)" "(.*?)"$/ do |firstName, lastName| 
+  visit "dashboard"
+  expect(page).to have_content(firstName)
+  expect(page).to have_content(lastName)
+end
+
+When /^I have attempted to sign in the user with email "(.*?)" and password "(.*?)" $/ do |email, password|
+  visit 'users/sign_in'
+  fill_in 'user_email', :with => email
+  fill_in 'user_password', :with => password
+  click_button 'Sign in'
+end
+
+Then /^I should be signed in with the email "(.*?)"$/ do |email| 
+  visit "dashboard"
+  expect(page).to have_content(email)
+end
+
+Then /^I should be able to view my properties$/ do
+  visit "dashboard"
+  click_on "Repairs"
+  expect(page).to have_content("Maintenance Issues page")
 end
